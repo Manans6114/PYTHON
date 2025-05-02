b6009ebb370686fec6affc9e135724a0375b1357
mnan <manansharma127021@gmail.com>
2025-05-02 05:59:17 +0530

    Updated Code

---
+++ Complete Course/Data Structures/06_tuple_methods.py
@@ -0,0 +1,4 @@
+t = (3, 12, 1, 54, 23, 12)
+
+print(t.count(12))     #Print the no. of accurrence of 12
+print(t.index(12))     #Return the index of the first occurenceo 12
---
+++ Complete Course/funcmethod/08_docstrings.py
@@ -0,0 +1,7 @@
+def sum(a, b):
+    '''This will sum two numbers'''
+    c = a + b
+    return c
+
+print(sum.__doc__)
+print(sum(2,3))
